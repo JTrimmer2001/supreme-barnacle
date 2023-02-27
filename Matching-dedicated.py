6385eed59a5472aef46a0cce9a17b76cced0f3bb
Joshua Trimmer <jet2609@gmail.com>
2023-02-27 10:50:15 +0000

    Added a column for data type Regenerated the data sets

--- Matching-dedicated.py
+++ Matching-dedicated.py
@@ -59,7 +59,10 @@ def newMatchset():
     need to filter a few times for all data, thats chill tho
     '''
 
-    master = pd.DataFrame(columns=('id','zpdf','mass')) # initialises an empty data frame for the sampled data to be appended to
+    master = pd.DataFrame(columns=('id','zpdf','mass','AGN or not')) # initialises an empty data frame for the sampled data to be appended to
+
+    agn_type = 0
+    gal_type = 1
 
     x=0
     for i_x in agn_histogram:
@@ -67,7 +70,7 @@ def newMatchset():
         for i_y in i_x: # These nested loops loop through the histogram to see if there are more or less agns
 
             if i_y == 0: # begins checking the amount of agns in the square with 0 (to speed up process)
-                print('\r','Skipped data at:',x,',',y,'                               matches made: ',matchesmade,'                 ',sep='',end='', flush = True)
+                print('\r','matches made: ',matchesmade,'; Skipped data at:',[x,y],'                    ',sep='',end='', flush = True)
                 time.sleep(0.1) # slows the skipping so you can see whats going on
 
             elif i_y < gal_histogram[x,y]:
@@ -84,11 +87,13 @@ def newMatchset():
                 gal_subset = gal_subset[gal_subset['mass'] <= ybins_gal[y+1]]
 
                 gal_sample = gal_subset.sample(int(i_y) )# Takes a random sample of data in the set to match the amount in the agn set
+                gal_sample['AGN or not'] = gal_type
+                agn_subset['AGN or not'] = agn_type
 
                 master = pd.concat([master,agn_subset,gal_sample], ignore_index=True, sort=False) # Concats the three tables together, ignoring index so thngs dont get confuse
                 matchesmade += 2*i_y
-                print('\r',i_y,'AGNs and', gal_histogram[x,y], 'Galaxies at',[x,y],'                 matches made: ',matchesmade,'  ',sep='',end='', flush = True)
-                time.sleep(0.1)
+                print('\r','matches made: ',matchesmade,'; ',i_y,' AGNs and ', gal_histogram[x,y], ' Galaxies at ',[x,y],'                    ',sep='',end='', flush = True)
+                time.sleep(0.05)
 
             else: # This method includes cases when i_y > gal_histogram[x,y], decided this was best until proven otherwise to maintain the amount of agns
 
@@ -102,17 +107,20 @@ def newMatchset():
                 gal_subset = gal_subset[gal_subset['mass'] >= ybins_gal[y]]
                 gal_subset = gal_subset[gal_subset['mass'] <= ybins_gal[y+1]]  
 
+                gal_subset['AGN or not'] = gal_type
+                agn_subset['AGN or not'] = agn_type
+
                 master = pd.concat([master, agn_subset, gal_subset], ignore_index=True, sort=False) # Concats tables, index are ignore as ID is the identifier
                 matchesmade += 2*i_y
-                print('\r',i_y,'AGNs and', gal_histogram[x,y], 'Galaxies at',[x,y],'                 matches made: ',matchesmade,'  ',sep='',end='', flush = True)
-                time.sleep(0.1) # A note for another time: this whole progress thing would look alot nicer with the matches made bit at the front (not of particularly variable width)
+                print('\r','matches made: ',matchesmade,'; ',i_y,' AGNs and ', gal_histogram[x,y], ' Galaxies at ',[x,y],'                    ',sep='',end='', flush = True)
+                time.sleep(0.05) # A note for another time: this whole progress thing would look alot nicer with the matches made bit at the front (not of particularly variable width)
 
             y+=1 # iterates y so the indices can be changed
 
         x+=1 
 
     master.info()
-    #master.to_csv('Matched sets/Matched data10.csv')
+    master.to_csv('Matched sets/Matched data10.csv')
 
 
 newMatchset()
