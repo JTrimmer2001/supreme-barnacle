63134eb5aed237e6581a2d2dd6ad75d7b802999f
Joshua Trimmer <jet2609@gmail.com>
2023-02-27 11:56:03 +0000

    Added a simple function to plot from the csv files produced in matched sets

---
+++ Plotting-script.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.io import fits
+import pandas as pd
+
+
+
+
+'''
+This is intended to be a graphing script, code may be compartmentalised into functions but the functions should be reuseable easily
+
+tables I want to do: Mass-SFR  
+                     mass-redshift (again) [done]
+                     Luminosity - redshift (maybe)
+'''
+
+def unifunction(x,y):
+    data = pd.read_csv('Matched sets/Matched data2.csv',header = 0, delimiter=',')
+
+    agns = data[data['AGN or not'] == 0]
+    gals = data[data['AGN or not'] == 1]
+
+    plt.scatter(x,y,c='b',s=2,data = agns)
+    plt.scatter(x,y,c='r',s=2,data = gals)
+
+    plt.xlabel(x)
+    plt.ylabel(y)
+
+    plt.show()
+
+
+unifunction('zpdf','mass')
